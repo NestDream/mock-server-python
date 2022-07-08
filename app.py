2b21ad76b56d2b66a29c853d68569c8dbaff1e3e
Li Guo <alexguo529@gmail.com>
2022-07-07 22:05:51 -0700

    add mocktest

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 app = Flask(__name__)
@@ -24,6 +26,25 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+@app.route('/mocktest', methods=['GET'])
+def mocktest():
+# [{
+#     "DataSetId": 1090898,
+#     "Id": 560881,
+#     "Saved_Date": "2021-06-30T20:08:07.680399",
+#     "Published_Date": "2021-06-30T18:07:52.715111",
+#     "Delivery_Date": "2006-12-31T13:00:00",
+#     "Value": 14.95
+#   },
+#   {
+#     "DataSetId": 1090898,
+#     "Id": 560882,
+#     "Saved_Date": "2021-06-30T20:08:07.680399",
+#     "Published_Date": "2021-06-30T18:07:52.715111",
+#     "Delivery_Date": "2006-12-31T13:30:00",
+#     "Value": 15.24
+#   }]
+    return json.dumps([{"DataSetId":1090898,"Id":560881,"Saved_Date":"2021-06-30T20:08:07.680399","Published_Date":"2021-06-30T18:07:52.715111","Delivery_Date":"2006-12-31T13:00:00","Value":14.95},{"DataSetId":1090898,"Id":560882,"Saved_Date":"2021-06-30T20:08:07.680399","Published_Date":"2021-06-30T18:07:52.715111","Delivery_Date":"2006-12-31T13:30:00","Value":15.24}])
 
 if __name__ == '__main__':
    app.run()
